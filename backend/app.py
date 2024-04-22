bb66fe6ca3459f7a7a93ef5d987510bcdc28fb5c
Rotich <jane.rotich@student.moringaschool.com>
2024-04-22 16:08:08 +0300

    seed db using curl

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -30,8 +30,42 @@ def users():
         users = Users.query.all()
         # gettin all values from db
         return jsonify([{"id": i.id, "username": i.username, "email": i.email, "password": i.pwd} for i in users])
+    elif (method.lower() == "post"):
+        try:
+            username = request.json["username"]
+            email = request.json["email"]
+            pwd = request.json["pwd"]
+            if (username and pwd and email):
+                try:
+                    user = Users(username, email, pwd)
+                    db.session.add(user)
+                    db.session.commit()
+                    return jsonify({"success": True})
+                except Exception as e:
+                    return ({"error": e})
+            else:
+                return jsonify({"error": "Invalid form"})
+        except:
+            return jsonify({"error": "Invalid form"})
+    elif (method.lower() == "delete"): # DESTROY
+        try:
+            uid = request.json["id"]
+            if (uid):
+                try:
+                    user = Users.query.get(uid) # Gets user with id = uid (because id is primary key)
+                    db.session.delete(user) # Delete the user
+                    db.session.commit() # Save
+                    return jsonify({"success": True})
+                except Exception as e:
+                    return jsonify({"error": e})
+            else:
+                return jsonify({"error": "Invalid form"})
+        except:
+            return jsonify({"error": "m"})
+
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
