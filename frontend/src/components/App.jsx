7386f65559dc4c5ca7e4397e8fc1a7367ecf0959
Rotich <jane.rotich@student.moringaschool.com>
2024-04-23 12:02:12 +0300

    feat:update login and register components

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React from "react";
 import Home from "./Home";
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
+import Login from "./Login";
+import Register from "./Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <React.Fragment>
+    return (
+        <React.Fragment>
             <Navbar />
-            <Home />
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </Router>
         </React.Fragment>
     );
 }
 
-export default App
+export default App;
